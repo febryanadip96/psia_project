97efde86ef1d5dd806910c994e23fe3d97a9e19e
AriPra97 <31333052+AriPra97@users.noreply.github.com>
2017-12-04 22:34:23 +0700

    New SI Laporan

--- app/Http/Controllers/User/LaporanController.php
+++ app/Http/Controllers/User/LaporanController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers\User;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use DB;
+use App\Akun;
 
 class LaporanController extends Controller
 {
@@ -14,6 +16,14 @@ class LaporanController extends Controller
 
     public function index()
     {
-    	return view('user.laporan.index');
+    	$akuns = Akun::all();
+    	$laporanJurnals = DB::table('vlaporanjurnal')->get();
+    	$arusKasList = DB::table('varuskas')->get();
+    	$pendapatans = DB::table('vlabarugi')->where('nomor', 'like', '4%')->get();
+    	$biayas = DB::table('vlabarugi')->where('nomor', 'like', '5%')->get();
+    	$perubahaEkuitasList = DB::table('vperubahanEkuitas')->get();
+    	$aktivas = DB::table('vneraca')->where('nomor', 'like', '1%')->get();
+    	$pasivas = DB::table('vneraca')->where('nomor', 'like', '2%')->get();
+    	return view('user.laporan.index',['akuns' => $akuns,'laporanJurnals' => $laporanJurnals, 'arusKasList' => $arusKasList,'perubahaEkuitasList' => $perubahaEkuitasList, 'pendapatans' => $pendapatans, 'biayas' => $biayas, 'aktivas' => $aktivas, 'pasivas' => $pasivas]);
     }
 }
--- resources/views/user/laporan/index.blade.php
+++ resources/views/user/laporan/index.blade.php
@@ -3,37 +3,155 @@
 @section('content')
 <div class="container">
     <ul class="nav nav-tabs">
-        <li class="active"><a data-toggle="tab" href="#laporan">Laporan</a></li>
-        <li><a data-toggle="tab" href="#terima">Terima</a></li>
-        <li><a data-toggle="tab" href="#bayar">Bayar</a></li>
+        <li class="active"><a data-toggle="tab" href="#laporanJurnal">Laporan Jurnal</a></li>
+        <li><a data-toggle="tab" href="#arusKas">Arus Kas</a></li>
+        <li><a data-toggle="tab" href="#laporanLabaRugi">Laporan Laba/Rugi</a></li>
+        <li><a data-toggle="tab" href="#perubahanEkuitas">Perubahan Ekuitas</a></li>
+        <li><a data-toggle="tab" href="#neraca">Neraca</a></li>
     </ul>
 
     <div class="tab-content">
-        <div id="pembelian" class="tab-pane fade in active">
-            <h3>Jurnal Umum</h3>
-    <table class="table table-striped">
-        <thead>
-            <tr>
-                <th>Tanggal</th>
-                <th>Keterangan Transaksi</th>
-                <th>Akun</th>
-                <th>Debet</th>
-                <th>Kredit</th>
-                <th>No Bukti</th>
-            </tr>
-        </thead>
-        <tbody>
-            
-        </tbody>
-    </table>
+        <div id="laporanJurnal" class="tab-pane fade in active">
+            <h3>Laporan Jurnal</h3>
+		    <table class="table table-striped">
+		        <thead>
+		            <tr>
+		                <th>Tanggal</th>
+		                <th>Keterangan Transaksi</th>
+		                <th>Akun</th>
+		                <th>Debet</th>
+		                <th>Kredit</th>
+		                <th>Nomor Bukti</th>
+		            </tr>
+		        </thead>
+		        <tbody>
+		        @foreach ($laporanJurnals as $laporanJurnal)
+					<tr>
+						<td>{{$laporanJurnal->tanggal}}</td>
+						<td>{{$laporanJurnal->keterangan}}</td>
+						<td>{{$laporanJurnal->NamaAkun}}</td>
+						<td>Rp. {{$laporanJurnal->Debet?number_format($laporanJurnal->Debet,0,',','.'):0}}</td>
+						<td>Rp. {{$laporanJurnal->Kredit?number_format($laporanJurnal->Kredit,0,',','.'):0}}</td>
+						<td>{{$laporanJurnal->NomorBukti?$laporanJurnal->NomorBukti:'-'}}</td>
+					</tr>
+				@endforeach            
+		        </tbody>
+		    </table>
         </div>
-        <div id="terima" class="tab-pane fade">
-            <h3>Terima</h3>
-            <p>Some content in menu 1.</p>
+        <div id="arusKas" class="tab-pane fade">
+            <h3>Arus Kas</h3>
+            <table class="table table-striped">
+		        <thead>
+		            <tr>
+		                <th>Nomor</th>
+		                <th>Nama Akun</th>
+		                <th>Saldo Akhir</th>
+		            </tr>
+		        </thead>
+		        <tbody>
+		        @foreach ($arusKasList as $arusKas)
+					<tr>
+						<td>{{$arusKas->nomor}}</td>
+						<td>{{$arusKas->NamaAkun}}</td>
+						<td>Rp. {{$arusKas->SaldoAkhir?number_format($arusKas->SaldoAkhir,0,',','.'):0}}</td>
+					</tr>
+				@endforeach            
+		        </tbody>
+		    </table>
         </div>
-        <div id="bayar" class="tab-pane fade">
-            <h3>Bayar</h3>
-            <p>Some content in menu 2.</p>
+        <div id="laporanLabaRugi" class="tab-pane fade">
+            <h3>Laporan Laba Rugi</h3>
+            <h4><b>Pendapatan</b></h4>
+            @php
+            	$totalPendapatan = 0;
+            @endphp
+	        @foreach ($pendapatans as $pendapatan)
+				<p class="col-xs-4">{{$pendapatan->NamaAkun}}</p>
+				<p class="col-xs-8">Rp. {{$pendapatan->SaldoAkhir?number_format($pendapatan->SaldoAkhir,0,',','.'):0}}</p>
+				@php
+					$totalPendapatan += ($pendapatan->SaldoAkhir*$akuns->where('nomor',$pendapatan->nomor)->first()->saldo_normal*-1);
+				@endphp
+			@endforeach
+			<p class="col-xs-4">TOTAL PENDAPATAN</p>
+			<p class="col-xs-8">Rp. {{$totalPendapatan?number_format($totalPendapatan,0,',','.'):0}}</p>
+		    <h4><b>Biaya</b></h4>
+		    @php
+            	$totalBiaya = 0;
+            @endphp
+	        @foreach ($biayas as $biaya)
+				<p class="col-xs-4">{{$biaya->NamaAkun}}</p>
+				<p class="col-xs-8">Rp. {{$biaya->SaldoAkhir?number_format($biaya->SaldoAkhir,0,',','.'):0}}</p>
+				@php
+					$totalBiaya += ($biaya->SaldoAkhir*$akuns->where('nomor',$biaya->nomor)->first()->saldo_normal);
+				@endphp
+			@endforeach
+			<p class="col-xs-4">TOTAL BIAYA</p>
+			<p class="col-xs-8">Rp. {{$totalBiaya?number_format($totalBiaya,0,',','.'):0}}</p>
+			<br>
+			<br>
+			<h4 class="col-xs-4"><b>LABA/RUGI</b></h4>
+			@php
+				$labaRugi = $totalPendapatan-$totalBiaya;
+			@endphp
+			<h4 class="col-xs-8"><b>Rp. {{($labaRugi)?number_format(($labaRugi),0,',','.'):0}}</b></h4>
+        </div>
+        <div id="perubahanEkuitas" class="tab-pane fade">
+            <h3>Perubahan Ekuitas</h3>
+            @php
+            	$modalPemilik = 0;
+            @endphp
+	        @foreach ($perubahaEkuitasList as $perubahanEkuitas)
+				<p class="col-xs-4">{{$perubahanEkuitas->NamaAkun}}</p>
+				<p class="col-xs-8">Rp. {{$perubahanEkuitas->SaldoAkhir?number_format($perubahanEkuitas->SaldoAkhir,0,',','.'):0}}</p>
+				@php
+					$modalPemilik += ($perubahanEkuitas->SaldoAkhir*$akuns->where('nomor',$perubahanEkuitas->nomor)->first()->saldo_normal*-1);
+				@endphp
+			@endforeach
+			<p class="col-xs-4">LABA/RUGI</p>
+			<p class="col-xs-8">Rp. {{($labaRugi)?number_format(($labaRugi),0,',','.'):0}}</p>
+			<p class="col-xs-4"><b>Modal Pemilik</b></p>
+			@php
+				$modalPemilik += $labaRugi;
+			@endphp
+			<p class="col-xs-8"><b>Rp. {{($modalPemilik)?number_format(($modalPemilik),0,',','.'):0}}</b></p>
+        </div>
+        <div id="neraca" class="tab-pane fade">
+            <h3>Laporan Neraca</h3>
+        	<h4 class="col-xs-2">Aktiva</h4>
+        	@php
+        		$totalAktiva = 0;
+        	@endphp
+        	<div class="col-xs-10">
+        		@foreach ($aktivas as $aktiva)
+					<p class="col-xs-4">{{$aktiva->NamaAkun}}</p>
+					<p class="col-xs-8">Rp. {{$aktiva->SaldoAkhir?number_format($aktiva->SaldoAkhir,0,',','.'):0}}</p>
+					@php
+						$totalAktiva += ($aktiva->SaldoAkhir*$akuns->where('nomor', $aktiva->nomor)->first()->saldo_normal);
+					@endphp
+				@endforeach
+				<p class="col-xs-4"><b>TOTAL AKTIVA</b></p>
+				<p class="col-xs-8"><b>Rp. {{$totalAktiva?number_format($totalAktiva,0,',','.'):0}}</b></p>
+        	</div>
+        	<h4 class="col-xs-2">Pasiva</h4>
+        	@php
+        		$totalPasiva = 0;
+        	@endphp
+        	<div class="col-xs-10">
+        		@foreach ($pasivas as $pasiva)
+					<p class="col-xs-4">{{$pasiva->NamaAkun}}</p>
+					<p class="col-xs-8">Rp. {{$pasiva->SaldoAkhir?number_format($pasiva->SaldoAkhir,0,',','.'):0}}</p>
+					@php
+						$totalPasiva += ($pasiva->SaldoAkhir*$akuns->where('nomor', $pasiva->nomor)->first()->saldo_normal*-1);
+					@endphp
+				@endforeach
+				<p class="col-xs-4">{{$akuns->find('301')->nama}}</p>
+				<p class="col-xs-8">Rp. {{$modalPemilik?number_format($modalPemilik,0,',','.'):0}}</p>
+				@php
+					$totalPasiva += $modalPemilik;
+				@endphp
+				<p class="col-xs-4"><b>TOTAL PASIVA</b></p>
+				<p class="col-xs-8"><b>Rp. {{$totalPasiva?number_format($totalPasiva,0,',','.'):0}}</b></p>
+        	</div> 
         </div>
     </div>
 </div>
